a5283ce5ce0fa004b991390c3a5890670c983041
Ben Partington <bdpartington@gmail.com>
2022-02-01 11:07:11 +0000

    Add the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,17 +1,19 @@
 require_relative '../lib/oystercard.rb'
 
 describe Oystercard do
-  describe "#top_up" do  
-    it "should add money to the oyster card" do
-      oystercard = Oystercard.new
-      expect(oystercard).to respond_to(:top_up).with(1).argument
+  subject(:oystercard) { described_class.new }
+    
+    it 'card has default balance of 0' do
+      expect(oystercard.balance).to eq(0)
     end
-  end
+  
+  # describe "#top_up" do  
+  #   it "should add money to the oyster card" do
+  #     oystercard = Oystercard.new
+  #     expect(oystercard).to respond_to(:top_up).with(1).argument
+  #   end
+  # end
 end
 
 
 
-# Notes on Stack Trace:
-# NameError - unitialized constant Oystercard
-# ./spec/oystercard_spec
-# rb:1:in
