835fe66d35f7b37928ab26a23e389c45465cd00d
Ben Partington <bdpartington@gmail.com>
2022-02-04 10:40:29 +0000

    Step 12: Added context blocks

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,30 +15,36 @@ describe Oystercard do
     it "should add money to card" do
       expect{ oystercard.top_up(5) }.to change{ oystercard.balance }.by(5)
     end
-
-    it "throw an error if balance exceeds £90" do
-      limit = Oystercard::LIMIT
-      oystercard.top_up(limit)
-      expect{ oystercard.top_up(1) }.to raise_error "balance exceeded limit of #{limit}"
+    
+    context "balance exceeds top up limit" do
+      it "should error" do
+        limit = Oystercard::LIMIT
+        oystercard.top_up(limit)
+        expect{ oystercard.top_up(1) }.to raise_error "balance exceeded limit of #{limit}"
+      end
     end
   end
   
   describe "#touch_in" do
-    it 'should remember the entry station after touch in' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      expect(oystercard.entry_station).to eq(:kings_cross)
+    context "After touch in" do
+      it 'should touch in' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        expect( oystercard.in_journey? ).to eq true
+      end
+      
+      it 'should remember the entry station' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        expect(oystercard.entry_station).to eq(:kings_cross)
+      end
     end
 
-    it 'should touch in' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      expect( oystercard.in_journey? ).to eq true
-    end
-
-    it 'should error if insufficient funds' do
-      min = Oystercard::MIN 
-      expect{ oystercard.touch_in(:kings_cross) }.to raise_error "insufficient funds, balance less than #{min}"
+    context "insufficient funds" do
+      it 'should error' do
+        min = Oystercard::MIN 
+        expect{ oystercard.touch_in(:kings_cross) }.to raise_error "insufficient funds, balance less than #{min}"
+      end
     end
   end
 
@@ -51,33 +57,36 @@ describe Oystercard do
   end
 
   describe "#touch_out" do
-    it 'should touch out' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      oystercard.touch_out(:bank)
-      expect( oystercard.in_journey? ).to eq false
-    end 
+    context "after touch out" do
+  
+      it 'should touch out' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        oystercard.touch_out(:bank)
+        expect( oystercard.in_journey? ).to eq false
+      end 
 
-    it 'should remember the exit station after touch out' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      oystercard.touch_out(:bank)
-      expect(oystercard.exit_station).to eq(:bank)
-    end
+      it 'should remember the exit station' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        oystercard.touch_out(:bank)
+        expect(oystercard.exit_station).to eq(:bank)
+      end
 
-    it 'should deduct on touch out' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      oystercard.touch_out(:bank)
-      min = Oystercard::MIN
-      expect{ oystercard.touch_out(:bank) }.to change{ oystercard.balance }.by(-min)
-    end
+      it 'should deduct funds' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        oystercard.touch_out(:bank)
+        min = Oystercard::MIN
+        expect{ oystercard.touch_out(:bank) }.to change{ oystercard.balance }.by(-min)
+      end
 
-    it 'should set entry station to nil after touch out' do
-      oystercard.top_up(5)
-      oystercard.touch_in(:kings_cross)
-      oystercard.touch_out(:bank)
-      expect(oystercard.entry_station).to eq(nil)
+      it 'should set entry station to nil' do
+        oystercard.top_up(5)
+        oystercard.touch_in(:kings_cross)
+        oystercard.touch_out(:bank)
+        expect(oystercard.entry_station).to eq(nil)
+      end
     end
   end
   
